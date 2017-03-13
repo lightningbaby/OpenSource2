99a3cecf3821960e99592e24b1a78f3e58a1c53e
konghonglady <lingxuecnscu@outlook.com>
2017-03-13 16:46:31 +0800

    这是一个里程碑

---
+++ app/src/main/java/com/opens/android/opensource/news/NewItemOne.java
@@ -0,0 +1,169 @@
+package com.opens.android.opensource.news;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.opens.android.opensource.R;
+import com.opens.android.opensource.fetchsource.ThumbnailDownloader;
+import com.opens.android.opensource.tweet.Tweet;
+import com.opens.android.opensource.tweet.TweetFetch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by ttc on 2017/3/9.
+ */
+
+public class NewItemOne {
+    private RecyclerView mPhotoRecyclerView;
+    private static final String TAG="NewsItemOne";
+    private List<Tweet> mItems = new ArrayList<>();
+    private ThumbnailDownloader<PhotoHolder> mPhotoHolderThumbnailDownloader;
+    private Context mContext;
+    private Fragment mFragment;
+
+
+    public NewItemOne(RecyclerView mRcycle, Context context, Fragment fragment){
+        mPhotoRecyclerView=mRcycle;
+        mContext=context;
+        mFragment=fragment;
+
+        new FetchItemsTask().execute();
+        Handler responseHandler=new Handler();
+        mPhotoHolderThumbnailDownloader=new ThumbnailDownloader<>(responseHandler);
+        mPhotoHolderThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap thumbnail) {
+                        Drawable drawable=new BitmapDrawable(mFragment.getResources(),thumbnail);
+                        photoHolder.bindDrawable(drawable);
+                    }
+                }
+        );
+        mPhotoHolderThumbnailDownloader.start();
+        mPhotoHolderThumbnailDownloader.getLooper();
+        Log.i(TAG,"Background thread started");
+
+
+        mPhotoRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        setupAdapter();
+
+    }
+    public void Destroy() {
+        mPhotoHolderThumbnailDownloader.clearQueue();
+        mPhotoHolderThumbnailDownloader.quit();
+        Log.i(TAG,"Backeground thread destroyed");
+    }
+//it is Void ,not void
+
+    /**
+     * doInBackground启用后台线程来获取数据，当后台完成任务后用onPostExecute更新UI
+     */
+    private class FetchItemsTask extends AsyncTask<Void,Void,List<Tweet>> {
+        @Override
+        protected List<Tweet> doInBackground(Void... voids) {
+//
+            return new TweetFetch().fetchItems();
+        }
+
+        /**
+         *当后台完成任务后用onPostExecute更新UI
+         */
+        @Override
+        protected void onPostExecute(List<Tweet> items) {
+            mItems = items;
+            setupAdapter();
+        }
+    }
+
+    //set view
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        //VH 里放 View组件
+        private ImageView mTweetPortraitImageView;
+        private TextView mAuthorTextView;
+        private TextView mTweetBodyTextView;
+        private TextView mTweetTimeView;
+        private TextView mCommentView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTweetPortraitImageView = (ImageView) itemView
+                    .findViewById(R.id.head);
+            mAuthorTextView= (TextView) itemView.findViewById(R.id.usr_id);
+            mTweetBodyTextView= (TextView) itemView.findViewById(R.id.title);
+            mTweetTimeView=(TextView)itemView.findViewById(R.id.time);
+            mCommentView=(TextView)itemView.findViewById(R.id.comment);
+
+
+        }
+        public void bindDrawable(Drawable drawable) {
+            mTweetPortraitImageView.setImageDrawable(drawable);
+
+        }
+        public void bindTweetOthers(Tweet tweet){
+            mAuthorTextView.setText(tweet.getAuthor().toString());
+            mTweetBodyTextView.setText(tweet.getTweetBody().toString());
+            mTweetTimeView.setText(tweet.getTweetPubDate().toString());
+            mCommentView.setText("评论："+tweet.getCommentCount().toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        //Adapter里放模型
+        private List<Tweet> mTweets;
+
+        public PhotoAdapter(List<Tweet> tweets) {
+            mTweets = tweets;
+        }
+
+        /**
+         * @param viewGroup
+         * @param viewType
+         * 这个函数是搞啥嘞？？？？？？？？？？？？
+         */
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.example_list_item, viewGroup, false);
+            return new PhotoHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
+            Tweet tweet = mTweets.get(position);
+//            Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
+//            photoHolder.bindDrawable(tweet.getPortraitUrl().);
+            mPhotoHolderThumbnailDownloader.queueThumbanail(photoHolder, tweet.getPortraitUrl());
+            photoHolder.bindTweetOthers(tweet);
+
+
+
+        }
+        @Override
+        public int getItemCount() {
+            return mTweets.size();
+        }
+    }
+
+    //关联adapter
+    private void setupAdapter() {
+         if (mFragment.isAdded()) {
+//            判断该Fragment是否已经与Activity关联
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+}
--- app/src/main/java/com/opens/android/opensource/news/NewsFragment.java
+++ app/src/main/java/com/opens/android/opensource/news/NewsFragment.java
@@ -36,60 +36,30 @@ import java.util.List;
  */
 
 public class NewsFragment  extends Fragment{
-    private TextView mTextView;
     private HorizontalScrollMenu hsm_container;
-
-
-    /****>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*****************/
-
-
-
     private List<RecyclerView> mPhotoRecyclerView=new ArrayList<RecyclerView>();
     private static final String TAG="TweetFetchFragment";
-    private List<Tweet> mItems = new ArrayList<>();
-    private ThumbnailDownloader<PhotoHolder> mPhotoHolderThumbnailDownloader;
 
 
-     /*************<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*********/
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);//need to learn more
-
-        /***>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*************/
-         new FetchItemsTask().execute();
-
-         Handler responseHandler=new Handler();
-
-         mPhotoHolderThumbnailDownloader=new ThumbnailDownloader<>(responseHandler);
-         mPhotoHolderThumbnailDownloader.setThumbnailDownloadListener(
-         new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
-        @Override
-        public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap thumbnail) {
-        Drawable drawable=new BitmapDrawable(getResources(),thumbnail);
-        photoHolder.bindDrawable(drawable);
-            }
-        });
-         mPhotoHolderThumbnailDownloader.start();
-         mPhotoHolderThumbnailDownloader.getLooper();
-         Log.i(TAG,"Background thread started");
-        /************* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<**/
+        Log.i(TAG,"Background thread started");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_news,container,false);
-    hsm_container = (HorizontalScrollMenu) v.findViewById(R.id.hsm_container);
-    hsm_container.setSwiped(true);
-    hsm_container.setAdapter(new MenuAdapter());
-
-    Log.v("news","*****************");
-    //Toast.makeText(this.getActivity(),"news",Toast.LENGTH_SHORT);
-    return v;
-}
+        hsm_container = (HorizontalScrollMenu) v.findViewById(R.id.hsm_container);
+        hsm_container.setSwiped(true);
+        hsm_container.setAdapter(new MenuAdapter());
+        Log.v("news","*****************");
+        return v;
+    }
 
 
-class MenuAdapter extends BaseAdapter
+    class MenuAdapter extends BaseAdapter
     {
         String[] names = new String[]
                 { "菜单一", "菜单二", "菜单三", "菜单四", "菜单五", "菜单六", "菜单七","菜单八" };
@@ -117,9 +87,21 @@ class MenuAdapter extends BaseAdapter
                 tv.setText(str);
                 views.add(v);
             }
-            for( RecyclerView recy:mPhotoRecyclerView){
+            int size = mPhotoRecyclerView.size();
+            for( int i=0;i<size;i++){
+                RecyclerView recy =mPhotoRecyclerView.get(i);
                 recy.setLayoutManager(new LinearLayoutManager(getActivity()));
-                setupAdapter(recy);
+                switch(i){
+                    case 0:
+                        new NewItemOne(recy,getActivity(),NewsFragment.this);
+                        break;
+                    case 1:
+                        break;
+                    case 3:
+                        break;
+                }
+
+
             }
             return views;
         }
@@ -133,111 +115,5 @@ class MenuAdapter extends BaseAdapter
 //                    Toast.LENGTH_SHORT).show();
 
         }
-
     }
-
-
-
-    /****>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*****/
-
-
-
-
-
-
-    private class FetchItemsTask extends AsyncTask<Void,Void,List<Tweet>> {
-        @Override
-        protected List<Tweet> doInBackground(Void... voids) {
-//
-            return new TweetFetch().fetchItems();
-        }
-
-
-        @Override
-        protected void onPostExecute(List<Tweet> items) {
-            mItems = items;
-            for( RecyclerView recy:mPhotoRecyclerView){
-                recy.setLayoutManager(new LinearLayoutManager(getActivity()));
-                setupAdapter(recy);
-            }
-
-        }
-    }
-
-    //set view
-    private class PhotoHolder extends RecyclerView.ViewHolder {
-        //VH 里放 View组件
-        private ImageView mTweetPortraitImageView;
-        private TextView mAuthorTextView;
-        private TextView mTweetBodyTextView;
-        private TextView mTweetTimeView;
-        private TextView mCommentView;
-
-        public PhotoHolder(View itemView) {
-            super(itemView);
-            mTweetPortraitImageView = (ImageView) itemView
-                    .findViewById(R.id.head);
-            mAuthorTextView= (TextView) itemView.findViewById(R.id.usr_id);
-            mTweetBodyTextView= (TextView) itemView.findViewById(R.id.title);
-            mTweetTimeView=(TextView)itemView.findViewById(R.id.time);
-            mCommentView=(TextView)itemView.findViewById(R.id.comment);
-
-
-        }
-        public void bindDrawable(Drawable drawable) {
-            mTweetPortraitImageView.setImageDrawable(drawable);
-
-        }
-        public void bindTweetOthers(Tweet tweet){
-            mAuthorTextView.setText(tweet.getAuthor().toString());
-            mTweetBodyTextView.setText(tweet.getTweetBody().toString());
-            mTweetTimeView.setText(tweet.getTweetPubDate().toString());
-            mCommentView.setText("评论："+tweet.getCommentCount().toString());
-        }
-    }
-
-    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
-        //Adapter里放模型
-        private List<Tweet> mTweets;
-
-        public PhotoAdapter(List<Tweet> tweets) {
-            mTweets = tweets;
-        }
-
-
-        @Override
-        public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(getActivity());
-            View view = inflater.inflate(R.layout.example_list_item, viewGroup, false);
-            return new PhotoHolder(view);
-        }
-        @Override
-        public void onBindViewHolder(PhotoHolder photoHolder, int position) {
-            Tweet tweet = mTweets.get(position);
-//            Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
-//            photoHolder.bindDrawable(tweet.getPortraitUrl().);
-            mPhotoHolderThumbnailDownloader.queueThumbanail(photoHolder, tweet.getPortraitUrl());
-            photoHolder.bindTweetOthers(tweet);
-
-
-
-        }
-        @Override
-        public int getItemCount() {
-            return mTweets.size();
-        }
-    }
-
-    //关联adapter
-    private void setupAdapter(RecyclerView mRecy) {
-        if (isAdded()) {
-//            判断该Fragment是否已经与Activity关联
-            mRecy.setAdapter(new PhotoAdapter(mItems));
-        }
-    }
-
-     /***<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*****/
-
-
-
 }
