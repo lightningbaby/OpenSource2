e7ee9487dfffba410b5ed99536cf1372714c2493
ExcaliburQAQ <1150790988@qq.com>
2017-03-14 15:54:30 +0800

    发布动弹和用户信息主界面

--- app/src/main/java/com/opens/android/opensource/mine/MineFragment.java
+++ app/src/main/java/com/opens/android/opensource/mine/MineFragment.java
@@ -1,25 +1,49 @@
 package com.opens.android.opensource.mine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.opens.android.opensource.R;
+import com.opens.android.opensource.shake.ShakeActivity;
 
 /**
  * Created by ttc on 2017/3/12.
  */
 
-public class MineFragment extends Fragment{
+public class MineFragment extends Fragment implements View.OnClickListener{
     private TextView mTextView;
+    private LinearLayout mShakeLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.fragment_recycleview,container,false);
-        mTextView=(TextView)v.findViewById(R.id.text1);
-        mTextView.setText("mine");
+        View v=inflater.inflate(R.layout.fragment_mine,container,false);
+        mShakeLayout=(LinearLayout)v.findViewById(R.id.rl_info_shake);
+        mShakeLayout.setOnClickListener(this);
+
+        //mTextView=(TextView)v.findViewById(R.id.text1);
+        //mTextView.setText("摇一摇");
+        /*
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(), ShakeActivity.class);
+                startActivity(intent);
+            }
+        });
+        */
         return v;
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.rl_info_shake:
+                Intent intent=new Intent(getActivity(), ShakeActivity.class);
+                startActivity(intent);
+        }
     }
 }
